c46c0a627d13948054fadf454d85b7cdd9641e51
samuelbianch <samuelbianch38@gmail.com>
2022-10-20 15:23:33 -0400

    Refatorando para .NET 5

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[Controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int Id = 1;
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = Id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperarFilmePorId), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperarFilme()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperarFilmePorId(int id)
+        {
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; internal set; }
+        [Required(ErrorMessage = "O campo Título é pbrigatório")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Diretor é obrigatório")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1, 400, ErrorMessage = "A duração deve ser entre 1 e 400 minutos")]
+        public int Duracao { get; set; }
+    }
+}
