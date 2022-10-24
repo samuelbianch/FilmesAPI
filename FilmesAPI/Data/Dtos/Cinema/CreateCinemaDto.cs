c85b765059a81f395fe45e02174ccf80c574f112
samuelbianch <samuelbianch38@gmail.com>
2022-10-24 17:15:38 -0400

    Add migrate que exclui endereco e gerente fk do database

--- FilmesAPI/Data/Dtos/Cinema/CreateCinemaDto.cs
+++ FilmesAPI/Data/Dtos/Cinema/CreateCinemaDto.cs
@@ -12,5 +12,7 @@ namespace FilmesAPI.Data.Dtos
         [Required(ErrorMessage = "O campo de nome é obrigatório")]
         public string Nome { get; set; }
 
+        public int EnderecoId { get; set; } 
+
     }
 }
---
+++ FilmesAPI/Migrations/20221024210948_Exclui endereco e gerente fk.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FilmesApi.Migrations
+{
+    public partial class Excluienderecoegerentefk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnderecoFK",
+                table: "Cinemas");
+
+            migrationBuilder.DropColumn(
+                name: "GerenteFK",
+                table: "Cinemas");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EnderecoFK",
+                table: "Cinemas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GerenteFK",
+                table: "Cinemas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
