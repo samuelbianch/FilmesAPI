bcc1cefd38382cadab3c29591ad64fec7920f489
samuelbianch <samuelbianch38@gmail.com>
2022-10-26 18:29:42 -0400

    Add: sessao getters and setters

---
+++ FilmesAPI/Controllers/SessaoController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using FilmesApi.Data;
+using FilmesApi.Data.Dtos.Sessao;
+using FilmesApi.Models;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SessaoController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public SessaoController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaSessao([FromBody] CreateSessaoDto sessaoDto)
+        {
+            Sessao sessao = _mapper.Map<Sessao>(sessaoDto);
+            _context.Sessoes.Add(sessao);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaSessoesPorId), new { Id = sessao.Id }, sessao);
+        }
+
+        [HttpGet]
+        public IEnumerable<Sessao> RecuperaSessoes()
+        {
+            return _context.Sessoes;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaSessoesPorId(int id)
+        {
+            Sessao sessao = _context.Sessoes.FirstOrDefault(sessao => sessao.Id == id);
+            if (sessao != null)
+            {
+                ReadSessaoDto sessaoDto = _mapper.Map<ReadSessaoDto>(sessao);
+                return Ok(sessaoDto);
+            }
+            return NotFound();
+        }
+
+
+    }
+}
---
+++ FilmesAPI/Data/Dtos/Sessao/CreateSessaoDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilmesApi.Data.Dtos.Sessao
+{
+    public class CreateSessaoDto
+    {
+        public int CinemaID { get; set; }
+        public int FilmeId { get; set; }
+        public DateTime HorarioDeEncerramento { get; set; }
+
+    }
+}
