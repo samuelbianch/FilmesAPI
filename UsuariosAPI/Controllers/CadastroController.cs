58d81baf742813824994bf918142288a9c10ba6a
samuelbianch <samuelbianch38@gmail.com>
2022-11-22 20:01:39 -0400

    Criando a API de usuario

---
+++ UsuariosAPI/Controllers/CadastroController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosAPI.Data.Dtos;
+
+namespace UsuariosAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CadastroController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CadastrarUsuario(CreateUsuarioDto createDto)
+        {
+            return Ok(createDto);
+        }
+    }
+}
---
+++ UsuariosAPI/Profiles/UsuarioProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Profiles
+{
+    public class UsuarioProfile : Profile
+    {
+        public UsuarioProfile()
+        {
+            CreateMap<CreateUsuarioDto, Usuario>();
+        }
+    }
+}
