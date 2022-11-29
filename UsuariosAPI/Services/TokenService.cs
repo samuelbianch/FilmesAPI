b46d804f50ae056b65eaeb62a857d334bbc38e26
samuelbianch <samuelbianch38@gmail.com>
2022-11-29 14:47:57 -0400

    Criando Service do Token)

---
+++ UsuariosAPI/Services/TokenService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Services
+{
+    public class TokenService
+    {
+        public Token CreateToken(IdentityUser<int> usuario)
+        {
+            Claim[] direitosUsuario = new Claim[]
+            {
+                new Claim("usarname", usuario.UserName),
+                new Claim("id", usuario.Id.ToString())
+            };
+
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(""));
+            var credenciais = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                claims: direitosUsuario,
+                signingCredentials: credenciais,
+                expires: DateTime.UtcNow.AddHours(1)
+                );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new Token(tokenString);
+
+        }
+    }
+}
